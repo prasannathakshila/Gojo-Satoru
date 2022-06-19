82b71c9ef7f080ab1c3cdf5304eafdd929f971dc
prasannathakshila <107807047+prasannathakshila@users.noreply.github.com>
2022-06-19 22:09:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94742403149']
+global.premium = ['94742403149']
+global.ownernomer = '94742403149'
+global.ownername = '🐦Thakshila Prasanna'
+global.botname = 'Rapix'
 global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
+global.ig = 'https://github.com/prasannathakshila'
 global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.sc = 'https://github.com/prasannathakshila/Gojo-Satoru'
+global.myweb = 'https://youtube.com/channel'
+global.packname = 'Rapix'
+global.author = '🐦Thakshila Prasanna'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
